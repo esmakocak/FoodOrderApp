9e942ec4becd03e5f04d93e205d6a30cb27cf92b
Esma Koçak <esma.kocaakk20@gmail.com>
2024-09-01 14:45:43 +0300

    Initial Commit

---
+++ FoodOrder/FoodOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodOrderApp.swift
+//  FoodOrder
+//
+//  Created by Esma Koçak on 1.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
